123b7d52935952d23dbfd68a486372fe8e84fa32
saurabh rajpoot <saurabh941193@gmail.com>
2023-07-07 15:14:47 +0530

    new folder mainApp created and header component moved to mainApp and rendring of item coded

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -1,13 +1,14 @@
 
 
- const Item =({image,description,titel,price})=>{
+ const Item =({titel,price,description,image})=>{
     return(
         <>
-        <section className="card">
-          <img src={image} className="image"></img>
+        <section className="card" >
+          <img src={image} className="image" alt="game-pic"></img>
           <article className="titel">{titel}</article>
           <article className="description">{description}</article>
           <article className="price">{price}</article>
+          <button className="add-button">Add to cart</button>
         </section>
         </>
     )
---
+++ src/components/mainApp/main.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+import Header from "../header/header"
+import axios from "axios";
+import Item from "../item/item"
+import "./main.css"
+const Main=()=>{
+    const [bestSaller,setBestSaller]=useState([]);
+    const [war,setWar]=useState([]);
+    useEffect(()=>{
+        axios.get('http://localhost:1337/api/best-sallers').then((best_saller)=>{
+               setBestSaller(best_saller.data.data);
+        }).catch((err)=>{
+            console.log(err)
+        }).finally(()=>{
+
+        })
+        axios.get('http://localhost:1337/api/wars').then((best_saller)=>{
+               setWar(best_saller.data.data);
+        }).catch((err)=>{
+            console.log(err)
+        }).finally(()=>{
+            
+        })
+    },[])
+    return(
+        <>
+        <Header />
+        <section>
+            {bestSaller.map((item,key)=>{
+                
+                return(
+                    <Item titel={item.attributes.Titel} price={item.attributes.price} description={item.attributes.Description} image={item.attributes.image} key={`${key} ${item.attributes.Titel}`} />
+                )
+            })}
+        </section>
+        </>
+    )
+}
+export default Main
